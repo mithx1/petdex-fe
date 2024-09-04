2f04478ff5106fb59f62dbbcb4f0f450091bc9f6
JULIUS CADUNGOG <104736@global.ul.com>
2024-09-04 21:17:02 +0800

    cleanup and install mui dependency

---
+++ petdex-fe/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Screen!</h1>
+      <p>This is the home screen of the application.</p>
+    </div>
+  );
+};
+
+export default HomePage;
