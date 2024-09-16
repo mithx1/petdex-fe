0cb06f57da5e87ae79c9b8c20a51ddbfbd33d223
Julius <71910804+mithx1@users.noreply.github.com>
2024-09-17 00:56:31 +0800

    Refactor HomePage component and update layout

--- petdex-fe/src/pages/HomePage.tsx
+++ petdex-fe/src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import SideNav from "../components/SideNav/SideNav";
-import { Box } from "@mui/material";
+import { Box, Card, CardContent, Typography } from "@mui/material";
 import Navbar from "../components/Appbar/Navbar";
+import Grid from "@mui/material/Grid";
+
+import Stack from "@mui/material/Stack";
 
 const HomePage: React.FC = () => {
   return (
@@ -11,7 +14,73 @@ const HomePage: React.FC = () => {
       <Box sx={{ display: "flex" }}>
         <SideNav />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <h1>Home</h1>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Stack spacing={2} direction="row">
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </Card>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Stack>
+            </Grid>
+            <Grid item xs={4}>
+              <Stack spacing={2}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                  </CardContent>
+                </Card>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Stack>
+            </Grid>
+          </Grid>
+          <Box height={20} />
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardContent></CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardContent></CardContent>
+              </Card>
+            </Grid>
+          </Grid>
         </Box>
       </Box>
     </>
