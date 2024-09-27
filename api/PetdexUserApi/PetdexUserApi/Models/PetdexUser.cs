cf018284e042f4949af2df0b4a3402e29e643720
Julius <71910804+mithx1@users.noreply.github.com>
2024-09-27 21:56:57 +0800

    api base for user registration

---
+++ api/PetdexUserApi/PetdexUserApi/Models/PetdexUser.cs
@@ -0,0 +1,12 @@
+﻿namespace PetdexUserApi.Models
+{
+    public class PetdexUser
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string EmailConfirmed { get; set; }
+    }
+}
